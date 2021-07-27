02e1efe4669587d5739aea5a54b2e89c37e1dfb0
owais897 <m.owais2118@gmail.com>
2021-07-27 20:43:16 +0500

    added skeleton in product page

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,38 +1,57 @@
 <template>
-  <a-space direction="vertical" size="large" style="padding: 50px">
-    <a-row :gutter="[20, 20]">
-      <a-col class="gutter-row" :span="6" v-for="item in items" :key="item.id">
-        <a-card @click="openThisProduct(item)" hoverable>
-          <img slot="cover" alt="example" :src="item.image" />
-          <a-card-meta>
-            <span slot="title">
-              <a-tooltip>
-                <template slot="title">
-                  {{ item.title }}
+  <div>
+    <div v-show="items.length > 0">
+      <a-space direction="vertical" size="large" style="padding: 50px">
+        <a-row :gutter="[20, 20]">
+          <a-col
+            class="gutter-row"
+            :span="6"
+            v-for="item in items"
+            :key="item.id"
+          >
+            <a-card @click="openThisProduct(item)" hoverable>
+              <img slot="cover" alt="example" :src="item.image" />
+              <a-card-meta>
+                <span slot="title">
+                  <a-tooltip>
+                    <template slot="title">
+                      {{ item.title }}
+                    </template>
+                    <span>{{ item.title.substring(0, 65) + '...' }}</span>
+                  </a-tooltip>
+                </span>
+                <template slot="description">
+                  <a-tooltip>
+                    <template slot="title">
+                      {{ item.description }}
+                    </template>
+                    <span>{{ item.description.substring(0, 65) + '...' }}</span>
+                    <br />
+                    <span class="price"> Price: {{ item.price }} </span>
+                  </a-tooltip>
                 </template>
-                <span>{{ item.title.substring(0, 65) + "..." }}</span>
-              </a-tooltip>
-            </span>
-            <template slot="description">
-              <a-tooltip>
-                <template slot="title">
-                  {{ item.description }}
-                </template>
-                <span>{{ item.description.substring(0, 65) + "..." }}</span>
-                <br />
-                <span class="price"> Price: {{ item.price }} </span>
-              </a-tooltip>
-            </template>
-          </a-card-meta>
-        </a-card>
-      </a-col>
-    </a-row>
-  </a-space>
+              </a-card-meta>
+            </a-card>
+          </a-col>
+        </a-row>
+      </a-space>
+    </div>
+    <div v-show="items.length === 0">
+      <template>
+        <a-row v-for="n in 4" :key="n" :gutter="[16, 32]">
+          <a-col :span="6"> <a-skeleton /> </a-col>
+          <a-col :span="6"> <a-skeleton /> </a-col>
+          <a-col :span="6"> <a-skeleton /> </a-col>
+          <a-col :span="6"> <a-skeleton /> </a-col>
+        </a-row>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import { apiMixin } from "../mixins";
-import axios from "axios";
+import { apiMixin } from '../mixins';
+import axios from 'axios';
 
 export default {
   created: async function () {
