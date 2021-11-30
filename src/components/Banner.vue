0965c8401f9fe7bd5e5d87f1f41c9f48127d8494
Muhammad Owais <m.owais2118@gmail.com>
2021-12-01 01:16:08 +0500

    change the banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,44 +1,45 @@
 <template>
   <div class="banner">
-    <a-carousel autoplay>
+    <a-carousel autoplay arrows>
+      <div
+        slot="prevArrow"
+        class="custom-slick-arrow"
+        style="left: 10px; z-index: 10"
+      >
+        <a-icon type="left-circle" />
+      </div>
+      <div slot="nextArrow" class="custom-slick-arrow" style="right: 10px">
+        <a-icon type="right-circle" />
+      </div>
       <div class="carousel-wrapper">
-        <img src="../assets/images/banner/banner.jpg" width="100%" alt="" />
+        <img
+          src="https://themepure.net/template/vue/vue/img/bg/bg.jpg"
+          width="100%"
+          alt=""
+        />
         <div>
-          <a-row type="flex" justify="start">
+          <a-row type="flex" justify="end" style="margin-right: 60px">
             <a-col :span="12">
-              <h2>New Brand One</h2>
+              <h2>Furniture</h2>
               <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Quaerat, id praesentium! Possimus quod qui voluptate dolore
-                suscipit et nostrum dicta vero dolores minus vel ullam obcaecati
-                quos dolor eos assumenda nesciunt iste enim, aut nobis
-                perferendis inventore corporis illum? Pariatur, obcaecati nam.
-                Officiis doloremque atque illo impedit odio cumque perspiciatis.
+                The vital part of every home is furniture, and it’s the first
+                thing to be considered while planning a home decor. Your home
+                furniture has to be presentable and classy, as it depicts your
+                values and mood. It also represents the first impression of your
+                home, and we are sure that you want it to look luxurious and
+                stylish. Always choose to buy furniture online in Pakistan from
+                reputed online furniture and decor store.
               </p>
             </a-col>
           </a-row>
         </div>
       </div>
-      <div>
-        <div class="carousel-wrapper">
-          <img src="../assets/images/banner/banner2.jpg" width="100%" alt="" />
-          <div>
-            <a-row type="flex" justify="start">
-              <a-col :span="12">
-                <h2>New Brand two</h2>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Quaerat, id praesentium! Possimus quod qui voluptate dolore
-                  suscipit et nostrum dicta vero dolores minus vel ullam
-                  obcaecati quos dolor eos assumenda nesciunt iste enim, aut
-                  nobis perferendis inventore corporis illum? Pariatur,
-                  obcaecati nam. Officiis doloremque atque illo impedit odio
-                  cumque perspiciatis.
-                </p>
-              </a-col>
-            </a-row>
-          </div>
-        </div>
+      <div class="carousel-wrapper">
+        <img src="../assets/images/banner/c1.jpeg" width="100%" alt="" />
+      </div>
+
+      <div class="carousel-wrapper">
+        <img src="../assets/images/banner/c2.jpeg" width="100%" alt="" />
       </div>
     </a-carousel>
   </div>
@@ -72,5 +73,21 @@ export default {
   position: absolute;
   top: 160px;
   left: 120px;
+}
+
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: rgb(2, 2, 2);
+  /* background-color: rgba(31, 45, 61, 0.11); */
+  opacity: 0.3;
+}
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel >>> .custom-slick-arrow:hover {
+  color: rgb(2, 2, 2);
+  opacity: 0.7;
 }
 </style>
